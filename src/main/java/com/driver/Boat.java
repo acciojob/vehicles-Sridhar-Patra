52e196b3192958f801073983cc51c631f16820c8
Sridhar-Patra <73095300+Sridhar-Patra@users.noreply.github.com>
2023-01-21 21:03:50 +0530

    3rd commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,16 +3,13 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
-
-    public Boat(String name ,int capacity){
-        this.name=name;
-        this.capacity=capacity;
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
@@ -20,10 +17,8 @@ public class Boat implements WaterVehicle{
     public String getVehicleName() {
         return this.name;
     }
-
     @Override
     public int getVehicleCapacity() {
         return this.capacity;
     }
-
 }
